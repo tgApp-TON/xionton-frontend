dcd76278c72c6e5804acdaa5139d4af915f8eabd
StanislavOzarchuk <dr.ozarchuks@gmail.com>
2026-02-10 03:30:26 +0100

    fix: convert next.config to pure JS

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    turbo: {},
+  },
 };
 
 export default nextConfig;
